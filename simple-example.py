d1efb118dcf1712dba20b32e917075e1f6559dcb
Baudouin Raoult <baudouin.raoult@ecmwf.int>
2018-05-12 10:13:27 +0100

    new example

---
+++ simple-example.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+
+import cdsapi
+import logging
+
+
+logging.basicConfig(level=logging.DEBUG,
+                    format='%(asctime)s %(levelname)s %(message)s')
+
+
+c = cdsapi.Client()
+
+
+c.retrieve("insitu-glaciers-elevation-mass",
+           {
+               "variable": "elevation_change",
+               "format": "tgz"
+           },
+           "dowload.data")
