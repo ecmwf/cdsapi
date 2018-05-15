a71e21abde0f45155c86d044bf609a7d37a891ba
Alessandro Amici <a.amici@bopen.eu>
2018-05-15 20:09:53 +0200

    Add first test and sync MANIFEST.

---
+++ tests/test_api.py
@@ -0,0 +1,11 @@
+
+from cdsapi import api
+
+
+def test_bytes_to_string():
+    assert api.bytes_to_string(1) == '1'
+    assert api.bytes_to_string(1 << 10) == '1K'
+    assert api.bytes_to_string(1 << 20) == '1M'
+    assert api.bytes_to_string(1 << 30) == '1G'
+    assert api.bytes_to_string(1 << 40) == '1T'
+    assert api.bytes_to_string(1 << 50) == '1P'
