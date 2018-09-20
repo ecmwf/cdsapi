7f29b56934f7857beb950bda01a69ba5033289ba
Gionata Biavati <Gionata.Biavati@ecmwf.int>
2018-09-20 09:23:57 +0100

    added __enter__ and __exit__ methods

--- cdsapi/api.py
+++ cdsapi/api.py
@@ -149,6 +149,13 @@ class Client(object):
 
     logger = logging.getLogger('cdsapi')
 
+    def __enter__(self, **kwargs):
+        self.__init__(**kwargs)
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        pass
+
     def __init__(self,
                  url=os.environ.get('CDSAPI_URL'),
                  key=os.environ.get('CDSAPI_KEY'),
