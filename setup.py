45aa190d2c37c3517c670c5b17a67a30e8ec4500
Mattia Almansi <m.almansi@bopen.eu>
2024-02-29 16:45:06 +0100

    remove branch from git url

--- setup.py
+++ setup.py
@@ -45,7 +45,7 @@ setuptools.setup(
     packages=setuptools.find_packages(),
     include_package_data=True,
     install_requires=[
-        "cads-api-client@git+https://github.com/ecmwf-projects/cads-api-client.git@COPDS-1531-beta-api",  # FIXME
+        "cads-api-client@git+https://github.com/ecmwf-projects/cads-api-client.git",
         "requests>=2.5.0",
         "tqdm",
     ],
