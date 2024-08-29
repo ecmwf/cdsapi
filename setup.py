213a765f2a4f6788e29fad4932815ade5c326037
Mattia Almansi <m.almansi@bopen.eu>
2024-08-29 16:56:29 +0200

    cads-api-client>=v1.3.0

--- setup.py
+++ setup.py
@@ -45,7 +45,7 @@ setuptools.setup(
     packages=setuptools.find_packages(),
     include_package_data=True,
     install_requires=[
-        "cads-api-client>=1.2.0",
+        "cads-api-client>=1.3.0",
         "requests>=2.5.0",
         "tqdm",
     ],
