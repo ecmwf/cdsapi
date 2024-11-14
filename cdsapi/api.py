2c17885c159030545c442ba5b2ba1f1c98ada446
malmans2 <m.almansi@bopen.eu>
2024-11-14 10:35:10 +0100

    use datapi

--- cdsapi/api.py
+++ cdsapi/api.py
@@ -22,6 +22,7 @@ try:
 except ImportError:
     from urlparse import urljoin
 
+from datapi.legacy_api_client import LegacyApiClient
 from tqdm import tqdm
 
 
@@ -281,9 +282,8 @@ class Client(object):
         _, token, _ = get_url_key_verify(url, key, None)
         if ":" in token:
             return super().__new__(cls)
-        import cads_api_client.legacy_api_client
 
-        return super().__new__(cads_api_client.legacy_api_client.LegacyApiClient)
+        return super().__new__(LegacyApiClient)
 
     def __init__(
         self,
